fc050ffbe590497c059b320c9f18b398c387b6ab
thienanle04 <lethienan1106@gmail.com>
2025-10-31 12:22:56 +0700

    js with react router reinit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
-});
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+})
